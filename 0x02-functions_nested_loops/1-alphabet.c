31cd5a83e9fa436f96b9bfca4140eb29453e7476
alazarabebe <alazarabebe9@gmail.com>
2023-02-22 19:08:39 -0800

    1-alphabe

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - prints putchar
+ * print_alphabet - prints putchar
  * Return: void
  */
 
 void print_alphabet(void)
 {
 	char c;
-	for (c = 'a'; c<= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
