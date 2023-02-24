302b426b3f2a7fb16dc6bef3936866a5ddde1fbd
alazarabebe <alazarabebe9@gmail.com>
2023-02-23 19:05:45 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,7 +8,7 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b , int c)
+int largest_number (int a, int b , int c)
 
 {
 
@@ -26,6 +26,5 @@ int largest_number(int a, int b , int c)
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
