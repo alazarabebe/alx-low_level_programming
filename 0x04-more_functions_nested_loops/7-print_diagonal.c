a8fa863e3dc1d6ca0d277b6daf8c1b10485245aa
alazarabebe <alazarabebe9@gmail.com>
2023-02-24 13:37:44 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check if its upper case
+ * @n : number of lines
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for  (j = 0; j < n; j++)
+		{
+			for (i = 0; i < n; i++)
+			{
+			_putchar(32);
+			}
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+}
