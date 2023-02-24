ba5842c3d05c6cd7226f668ea4323f12b9e14fed
alazarabebe <alazarabebe9@gmail.com>
2023-02-24 11:30:06 -0800

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#include <ctype.h>
+
+/**
+ * _isupper - check if its upper case
+ * Return: integer
+ */
+
+int _isupper(int c)
+{
+	int res;
+
+	res = isupper(c);
+	return res;
+}
