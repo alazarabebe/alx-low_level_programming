7ecd2e569b7c77218a8a918f97c1f5f656367e25
alazarabebe <alazarabebe9@gmail.com>
2023-02-24 11:59:12 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+#include <ctype.h>
+
+/**
+ * _isupper - check if its upper case
+ * @c: input char
+ * Return: integer
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c < 58)
+	{
+	return (1);
+	}
+	return (0);
+}
