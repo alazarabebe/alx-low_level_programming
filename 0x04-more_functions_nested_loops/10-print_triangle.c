402b591e475b769299b7eef2118b3a0492892303
alazarabebe <alazarabebe9@gmail.com>
2023-02-24 19:03:28 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_square - check if its upper case
+ * @size : number of lines
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for  (i = 0; i < size; i++)
+		{
+			for (j = size - x; j > 1; j--)
+			{
+			_putchar(32);
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(35);
+			}
+		_putchar('\n');
+		}
+	}
+}
