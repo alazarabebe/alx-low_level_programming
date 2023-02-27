d85308783fb90ad16c0370be4ba9b1334da9e954
alazarabebe <alazarabebe9@gmail.com>
2023-02-27 13:00:57 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int  - check if its upper case
+ * @a: number
+ * @b: number
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+
+	*b = 98;
+
+}
