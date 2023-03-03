b93e0309bd485141e838254ea61268625bc24a19
alazarabebe <alazarabebe9@gmail.com>
2023-03-02 17:56:57 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,8 +15,8 @@ char *_strncpy(char *dest, char *src, int n)
 	while (src[i++])
 		src_len++;
 	for (i = 0; src[i] && i < n; i++)
-		dest[dest_len++] = src[i];
-	for (i = src_len; index < n; i++)
-		dest[i] = '\0';
+		src[src_len++] = src[i];
+	for (i = src_len; i < n; i++)
+		src[i] = '\0';
 	return (dest);
 }
