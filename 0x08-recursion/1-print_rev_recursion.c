f0620f1ecbc414c9ee134fc7ad4ae4c469e6cab0
alazarabebe <alazarabebe9@gmail.com>
2023-03-08 12:18:23 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - puts
+ * _print_rev_recursion - puts
  * @s: array
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	size_t i = 0;
-
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
-	_puts_recursion(s + i);
-	i++;
-	return (i)
+	s++;
+	_puts_recursion(s);
+	s--;
+	_putchar(*s);
 }
