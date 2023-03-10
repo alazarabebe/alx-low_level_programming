6bcc25c5009b0d99f7a0503949d6268bd8f7fb71
alazarabebe <alazarabebe9@gmail.com>
2023-03-10 15:44:15 -0800

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - args
+ * @argc: count
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s \n",argv[argc - argc]);
+	return (0);
+}
