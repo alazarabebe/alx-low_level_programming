ec1ae71517374a93f181901b28d3938a14066d3e
alazarabebe <alazarabebe9@gmail.com>
2023-03-10 18:35:40 -0800

    6

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,31 +24,27 @@ int main(int argc, char *argv[])
 	while (cents > 0)
 	{
 		coins++;
-		if((cents - 25) >= 0)
+		if ((cents - 25) >= 0)
 		{
 			cents -= 25;
 			continue;
 		}
-		if((cents - 10) >= 0)
+		if ((cents - 10) >= 0)
 		{
 			cents -= 10;
 			continue;
 		}
-		if((cents - 5) >= 0)
+		if ((cents - 5) >= 0)
 		{
 			cents -= 5;
 			continue;
 		}
-		if((cents - 2) >= 0)
+		if ((cents - 2) >= 0)
 		{
 			cents -= 2;
 			continue;
 		}
-		if((cents -1) >= 0)
-		{
-			cents -= 1;
-
-		}
+		cents -= 1;
 	}
 	printf("%d\n", coins);
 
