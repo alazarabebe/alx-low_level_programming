195a292ada19def70081fab116e4e240d72a89bc
alazarabebe <alazarabebe9@gmail.com>
2023-03-10 17:45:15 -0800

    5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - args
+ * @argc: count
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int  b, c;
+
+	for (c = 1; c < argc; c++)
+	{
+	if (atoi(argv[c]) != 0)
+	{
+		b += atoi(argv[c]);
+	}
+	else if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else if (atoi(argv[c]) == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	}
+	printf("%d\n", b);
+	return (0);
+}
