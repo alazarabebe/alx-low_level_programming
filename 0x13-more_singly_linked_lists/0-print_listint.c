fddbcf19eb61650ca8b1a509803599b7e6d4a31d
alazarabebe <alazarabebe9@gmail.com>
2023-04-04 19:17:19 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	while(h)
+	while (h)
 	{
 		i++;
-		printf("%d\n", h -> n);
-		h = h ->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (i);
 }
