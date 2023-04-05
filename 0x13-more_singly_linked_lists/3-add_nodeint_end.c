0f5d30b54d0c3a656c0d6215566bdc1e1561910c
alazarabebe <alazarabebe9@gmail.com>
2023-04-04 20:54:27 -0700

    2 and 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,24 +3,32 @@
 #include <stdio.h>
 
 /**
- * add_nodeint - prints the list
+ * add_nodeint_end - prints the list
  * @head: list pointer
  * @n: data
  *
  * Return: number of nodes
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n);
 {
 	listint_t *new;
+	list *temp = *head;
 
 	new = malloc(sizeof(listint_t));
 
 	if (!new)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
+	new->next = NULL;
+	if (*head == NULL)
+	{
 	*head = new;
+	return (new);
+	}
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
 
 	return (new);
 }
